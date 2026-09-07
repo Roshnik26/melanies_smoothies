5d74bd71e8abf997647959153bd04a9f65004f4f
Roshni <147968944+Roshnik26@users.noreply.github.com>
2026-09-07 13:49:30 +0530

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,44 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f"Customize your Smoothie")
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+
 st.write(
-  """Replace this example with your own code!
-  **And if you're new to Streamlit,** check
-  out our easy-to-follow guides at
-  [docs.streamlit.io](https://docs.streamlit.io).
-  """
+    """
+    Choose the fruits you want in your custom Smoothie!
+    """
 )
+
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+
+cnx = st.connection("snowflake")
+session = cnx.session()
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    my_dataframe,
+    max_selections=5
+)
+
+if ingredients_list:
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+    st.write(ingredients_string)
+
+    my_insert_stmt = """INSERT INTO smoothies.public.orders
+        (ingredients, name_on_order)
+        VALUES (?, ?)
+    """
+
+    session.sql(
+        my_insert_stmt,
+        params=[ingredients_string, name_on_order]
+    ).collect()
