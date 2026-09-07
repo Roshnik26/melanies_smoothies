22769bf857d7ecf30ecccabe364f5b6cf6a9b348
Roshni <147968944+Roshnik26@users.noreply.github.com>
2026-09-07 14:33:19 +0530

    Add SmoothieFroot API call
    Added API call to fetch watermelon data from Smoothiefroot.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -42,3 +43,11 @@ if ingredients_list:
         my_insert_stmt,
         params=[ingredients_string, name_on_order]
     ).collect()
+
+
+# Step 2 - API code
+smoothiefroot_response = requests.get(
+    "https://my.smoothiefroot.com/api/fruit/watermelon"
+)
+
+st.text(smoothiefroot_response)
